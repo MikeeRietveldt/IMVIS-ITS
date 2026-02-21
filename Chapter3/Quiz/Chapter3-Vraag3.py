98974eee3bbf900c5b04a84f3feecfcb169e1050
Mike <102684812+MikeeRietveldt@users.noreply.github.com>
2026-02-21 03:41:47 +0100

    Add files via upload

---
+++ Chapter3/Quiz/Chapter3-Vraag3.py
@@ -0,0 +1,14 @@
+# ITS IMVIS
+# Chapter 3 - 3d point maken
+# Mike Rietveldt -- 20183380
+
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2 
+
+
+Sx, Sy, Sz = 2,2,2 # Variabelen 
+S = np.array([[Sx, 0, 0],[0, Sy, 0], [0, 0, Sz]]) # 3d array aanmaken 
+
+print(S)
+
