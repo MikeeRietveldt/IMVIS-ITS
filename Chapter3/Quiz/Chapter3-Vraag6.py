9efabf0c2e6cc789880f30c06c0ab670dcd8e504
Mike <102684812+MikeeRietveldt@users.noreply.github.com>
2026-02-25 23:25:49 +0100

    Add files via upload

---
+++ Chapter3/Quiz/Chapter3-Vraag6.py
@@ -0,0 +1,10 @@
+# ITS IMVIS
+# Chapter 3 - floor,ceil,round + waarom forward mapping donkerder dan reverse transformation
+# Mike Rietveldt -- 20183380
+
+# Om te beginnen zijn dit  alle 3 afrond functies
+# Floor = afronden naar beneden
+# Ceil = Afronden naar boven
+# Round = Afronden naar dichtsbijzijnde gehele getal.
+
+# Forward mapping is donkerder aangezien je alleen de even pixels opvult, de oneven pixels blijven zwart
