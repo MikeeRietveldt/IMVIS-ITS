0e6de55f7f22e8d2b9ef8e3f2299b13acd00e1f4
Mike <102684812+MikeeRietveldt@users.noreply.github.com>
2026-03-24 05:26:51 +0100

    Add files via upload

---
+++ Chapter5/Quiz/Chapter5-Vraag1.py
@@ -0,0 +1,6 @@
+# ITS IMVIS
+# Chapter 5 - Verschil tussen affine en projective transformation matrix
+# Mike Rietveldt - 20183380
+
+# Projective voegt een perspectief effect toe, dit gebeurd niet bij affine transformations. Affine zorgt juist dat het paralel blijft.
+# De rechte lijnen bij projective zijn dus niet altijd evenwijdig
---
+++ Chapter5/Quiz/Chapter5-Vraag2.py
@@ -0,0 +1,11 @@
+# ITS IMVIS
+# Chapter 5 - Vraag 2 - 2x2 matrix en 2x1 matrix berekenen
+# Mike Rietveldt - 20183380
+ 
+# 2x2 rijen worden kolomen
+# [ 1  2 ]  [ 1  3 ]        
+# [ 3  4 ]  [ 2  4 ]
+
+# 2x1 kolom wordt rij
+# [ 1 ]  [ 1 2 ]
+# [ 2 ]
